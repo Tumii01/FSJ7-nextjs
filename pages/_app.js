eee759c0689fec0ed7dd9a82bb869655552e7984
Tumii01 <tumisangnke@gmail.com>
2023-10-02 09:41:02 +0200

    data addition

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
+import Head from 'next/head';
+
 import "../styles/globals.css";
 import Layout from "../components/layout/layout";
 function App({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <meta name='viewport'  content="width=device-width, intial-scale=1" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { Fragment } from "react";
+import Head from 'next/head'
 
 import Hero from "../components/home-page/hero";
 import FeaturedPosts from "../components/home-page/featured-posts";
@@ -8,6 +9,10 @@ import { getFeaturedPosts } from '../lib/posts-util';
 function HomePage(props) {
   return (
     <Fragment>
+      <Head>
+        <title>Tumii's Blog!</title>
+      <meta name='description' content="I post about Programming and web Development" />
+      </Head>
       <Hero />
       <FeaturedPosts posts={props.posts} />
     </Fragment>
